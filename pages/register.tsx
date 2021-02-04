d6cb67aabd3da5dca2557f2ec490e8b79af29d89
Varun Mk <mkvarun2001@gmail.com>
2021-02-04 15:04:02 +0530

    Added Server

--- pages/register.tsx
+++ pages/register.tsx
@@ -6,19 +6,28 @@ import { Link as L } from '@chakra-ui/react';
 import axios from 'axios';
 
 const Register = () => {
+    const [formData,setformData]=useState({name:"",email:""})
+
+    const handleChange=(event)=>{
+        setformData({...formData,[event.target.name]:event.target.value})
+    }
     
-    const onSubmit = async (values: any) => {
-        await axios
-            .post('http://localhost:4000/api/register', values)
+    const onSubmit = async (event) => {
+        await axios.post('http://localhost:4000/api/register',formData)
             .catch(function (err: any) {
                 console.log(err);
                 alert('Some error while signing up.');
             });
+        event.preventDefault();
     };
 
     return (
         <>
-
+        <form onSubmit={onSubmit}>
+            <input name="name" onChange={handleChange}/>
+            <input name="email" onChange={handleChange}/>
+            <button>Submit</button>
+        </form>
         </>
     );
 };
