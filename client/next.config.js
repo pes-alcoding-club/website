df036e7ae32d6921dc8f82ad8cf631923e978586
souravtecken <souravtecken@gmail.com>
2021-02-06 13:34:10 +0530

    Added github actions

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,13 @@
+const isProduction = (process.env.NODE_ENV || 'production') === 'production';
+
+const assetPrefix = isProduction ? '/website' : '';
+
 module.exports = {
+    trailingSlash: true,
+    assetPrefix: assetPrefix,
+    images: {
+        loader: "imgix"
+    },
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/,
