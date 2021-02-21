ec90f61b15ef999deec5531711b1e8677ea434f7
Prithvi Anil K <thepirateking3d2y@gmail.com>
2021-02-21 18:45:31 +0530

    removed fanim

--- client/src/components/contests/ContestCard.tsx
+++ client/src/components/contests/ContestCard.tsx
@@ -8,7 +8,6 @@ import {
     Stack,
     Text,
 } from '@chakra-ui/react';
-import { motion } from 'framer-motion';
 import React from 'react';
 
 export interface ContestCardProps {
@@ -26,77 +25,67 @@ const ContestCard: React.FC<ContestCardProps> = ({
     problemsURL,
     solutionURL,
 }) => (
-    <motion.div
-        initial={{ rotate: 180, scale: 0 }}
-        animate={{ rotate: 0, scale: 1 }}
-        transition={{
-            type: 'spring',
-            stiffness: 360,
-            damping: 30,
+    <Box
+        pos="relative"
+        display="flex"
+        flexDirection="column"
+        minW="0"
+        wordBreak="break-word"
+        backgroundColor="#fff"
+        backgroundClip="border-box"
+        border="1px solid rgba(0,0,0,.125)"
+        borderRadius=".25rem"
+        p="5"
+        w="60%"
+        h="100%"
+        mx="auto"
+        boxShadow="lg"
+        transition={{ lg: 'ease-out', base: 'none' }}
+        transitionDuration={{ lg: '.3s', base: 'none' }}
+        _hover={{
+            transform: 'translate(0, -7.5px)',
         }}
     >
-        <Box
-            pos="relative"
-            display="flex"
-            flexDirection="column"
-            minW="0"
-            wordBreak="break-word"
-            backgroundColor="#fff"
-            backgroundClip="border-box"
-            border="1px solid rgba(0,0,0,.125)"
-            borderRadius=".25rem"
-            p="5"
-            w="60%"
-            h="100%"
-            mx="auto"
-            boxShadow="lg"
-            transition={{ lg: 'ease-out', base: 'none' }}
-            transitionDuration={{ lg: '.3s', base: 'none' }}
-            _hover={{
-                transform: 'translate(0, -7.5px)',
-            }}
-        >
-            <Heading fontSize="3xl">{name}</Heading>
-            <Box my="3">
-                <Stack my="3" direction="row">
-                    {tags.map(({ tag, color }) => (
-                        <Badge
-                            key={tag}
-                            variant="solid"
-                            colorScheme={color}
-                            borderRadius="sm"
-                            fontWeight="bold"
-                        >
-                            {tag}
-                        </Badge>
-                    ))}
-                </Stack>
-                <Text color="gray.500">{date}</Text>
-            </Box>
-            <ButtonGroup>
-                <Link href={problemsURL} _hover={{ textDecoration: 'none' }}>
-                    <Button
-                        size="sm"
-                        fontSize="sm"
-                        colorScheme="teal"
+        <Heading fontSize="3xl">{name}</Heading>
+        <Box my="3">
+            <Stack my="3" direction="row">
+                {tags.map(({ tag, color }) => (
+                    <Badge
+                        key={tag}
                         variant="solid"
+                        colorScheme={color}
+                        borderRadius="sm"
+                        fontWeight="bold"
                     >
-                        Problems
-                    </Button>
-                </Link>
-                <Link href={solutionURL} _hover={{ textDecoration: 'none' }}>
-                    <Button
-                        size="sm"
-                        fontSize="sm"
-                        colorScheme="teal"
-                        variant="outline"
-                    >
-                        Solutions
-                    </Button>
-                </Link>
-            </ButtonGroup>
+                        {tag}
+                    </Badge>
+                ))}
+            </Stack>
+            <Text color="gray.500">{date}</Text>
         </Box>
-    </motion.div>
+        <ButtonGroup>
+            <Link href={problemsURL} _hover={{ textDecoration: 'none' }}>
+                <Button
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="teal"
+                    variant="solid"
+                >
+                    Problems
+                </Button>
+            </Link>
+            <Link href={solutionURL} _hover={{ textDecoration: 'none' }}>
+                <Button
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="teal"
+                    variant="outline"
+                >
+                    Solutions
+                </Button>
+            </Link>
+        </ButtonGroup>
+    </Box>
 );
 
 export default ContestCard;
